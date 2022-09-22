3d168a7b0949e2a0259e15d431b83298e984859e
unohayato <mamidori1p1p@gmail.com>
2022-09-22 19:53:14 +0900

    done

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "text/html"});
   res.write(html);
   res.end();
+
+  if (req.method == "GET") {
+
+  }
+  if (req.method == "POST") {
+
+  }
 });
+
 
 server.listen(PORT, () => {
   console.log("server runnning!");
