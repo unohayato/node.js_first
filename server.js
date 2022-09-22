5564ccb328783530ca2d55e7a94614bb159cf3a8
unohayato <mamidori1p1p@gmail.com>
2022-09-22 19:51:22 +0900

    nodemon

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const http = require("http");
 const PORT = 8000;
+const html = require("fs").readFileSync("./index.html")
 
 ///ウェブサーバーを作る
 const server = http.createServer((req, res) => {
   //ブラウザからアクセスが来たときの処理
   res.writeHead(200, {"Content-Type": "text/html"});
-  res.write("<h1>Hello World</h1>");
+  res.write(html);
   res.end();
 });
 
